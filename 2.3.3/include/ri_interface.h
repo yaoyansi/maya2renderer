8a988c07ea7ffb4042883a1725d54afe2bf09dd5
yaoyansi <yaoyansi2@yahoo.com.cn>
2011-07-01 10:26:35 +0800

    [refactoring]include "../riblib/ri.h"

--- 2.3.3/include/ri_interface.h
+++ 2.3.3/include/ri_interface.h
@@ -1,7 +1,15 @@
 #ifndef _RI_INTERFACE_H_
 #define _RI_INTERFACE_H_
 
-#include <ri.h>
-#pragma comment(lib,"../../bin/win32lib/rilib_d.lib")
+//#define RIBLIB_AQSIS
+
+#ifdef RIBLIB_AQSIS
+	
+//#elif  defined()
+#else 
+#	include "../riblib/ri.h"
+#	pragma comment(lib,"../../bin/win32lib/rilib_d.lib")
+#endif
+
 
 #endif
