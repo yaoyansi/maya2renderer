dce4a0b2980b10a48626f3226ed08b6246823863
yaoyansi <yaoyansi2@yahoo.com.cn>
2011-12-25 21:43:11 +0800

    [er] export lambert.color test case: er_shader_lambert.ma

--- 2.3.3/src/elvishray/er_shader.cpp
+++ 2.3.3/src/elvishray/er_shader.cpp
@@ -1,13 +1,22 @@
 
 #include "er_renderer.h"
-
+// Maya headers
+#include "../log/prerequest_maya.h"
+// Liquid headers
 #include <liqShader.h>
 
+#include "../log/mayacheck.h"
 #include "ercall.h"
 #include "log_helper.h"
 
 namespace elvishray
 {
+	//////////////////////////////////////////////////////////////////////////
+	void getDagPathByName(MDagPath &dagPath, const char *name);
+	void getDependNodeByName(MObject &depNode, const char *name);
+	void getShaderType(MString &type, const MString &name);
+	void shader_surface_lambert(const MString &mayaShaderName);
+	//////////////////////////////////////////////////////////////////////////
 	void Renderer::shader_transformBegin(const liqString  shaderSpace)
 	{
 
@@ -21,16 +30,28 @@ namespace elvishray
 		const std::vector<liqTokenPointer> &tokenPointerArray
 	)
 	{
-		//_s( "// shader_surface("<<shader.getShaderFileName()<<","<<", ...)" );//e.g."your_shader_dir/test_type2"
-		_s( "// shader_surface("<<shader.getName()<<","<<", ...)" );//e.g."lambert1", or "liquidSurface1"
+		const std::string &liquidShaderName=shader.getName();
+		const std::string &rmSloFilePath=shader.getShaderFileName();
+		const std::string &mayaShaderName=rmSloFilePath.substr(rmSloFilePath.find_last_of('/')+1);
+		_s( "// shader_surface("<<mayaShaderName<<","<<", ...)" );//Renderman slo file name, e.g."your_shader_dir/test_type2"
+		//_s( "// shader_surface("<<liquidShaderName<<","<<", ...)" );//e.g."lambert1", or "liquidSurface1", NOTE: it is liquidShader, not maya shader.
 
+		MObject shaderNode;
+		getDependNodeByName(shaderNode, mayaShaderName.c_str());
+		if(shaderNode.hasFn(MFn::kLambert)){
+			shader_surface_lambert(mayaShaderName.c_str());
+		}
+// 		MString shaderType;
+// 		getShaderType(shaderType, mayaShaderName.c_str());
+// 		if(shaderType=="lambert"){
+// 			shader_surface_lambert(mayaShaderName);
+// 		}
 		
 		size_t parameterNum =  tokenPointerArray.size() - 1;
 		for(size_t i=0; i<parameterNum; ++i)
 		{
 // 			_s("//- "
-// 				<<const_cast<liqTokenPointer*>(&tokenPointerArray[i])->
-// 				getDetailedTokenName()<<","//uniform float intensity
+// 				<<const_cast<liqTokenPointer*>(&tokenPointerArray[i])->getDetailedTokenName()<<","//uniform float intensity
 // 				<<tokenPointerArray[i].getDetailType()<<","
 // 				<<"//tokenPointerArray[i].getTokenFloatArray()"<<","
 // 				<<"//[error]tokenPointerArray[i].getTokenString()"<<","
@@ -81,23 +102,23 @@ namespace elvishray
 				assert(0);
 			}
 		}//for
-		const std::string shaderobject(shader.getName()+"_object");
-		_S( ei_shader( shaderobject.c_str() ));
-		_S( ei_shader_param_string("desc", "plastic") );
-		//_S( ei_shader_param_vector("Ka", 1.0f, 1.0f, 1.0f) ); 
-		_S( ei_shader_param_vector("Kd", 0.7f, 1.0f, 1.0f) ); 
-		_S( ei_shader_param_scalar("Ks", 1.0f) );
-		_S( ei_shader_param_scalar("roughness", 0.2f ) );
-		_S( ei_end_shader() );
-
-		_S( ei_shader("opaque_shadow"));
-		_S( ei_shader_param_string("desc", "opaque"));
-		_S( ei_end_shader());
-
-		_S( ei_material( shader.getName().c_str() ) );
-		_S( ei_add_surface(	shaderobject.c_str() ) );
-		//_S( ei_shadow( "opaque_shadow" ) );
-		_S( ei_end_material() );
+// 		const std::string shaderobject(shader.getName()+"_object");
+// 		_S( ei_shader( shaderobject.c_str() ));
+// 		_S( ei_shader_param_string("desc", "plastic") );
+// 		//_S( ei_shader_param_vector("Ka", 1.0f, 1.0f, 1.0f) ); 
+// 		_S( ei_shader_param_vector("Kd", 0.7f, 1.0f, 1.0f) ); 
+// 		_S( ei_shader_param_scalar("Ks", 1.0f) );
+// 		_S( ei_shader_param_scalar("roughness", 0.2f ) );
+// 		_S( ei_end_shader() );
+// 
+// 		_S( ei_shader("opaque_shadow"));
+// 		_S( ei_shader_param_string("desc", "opaque"));
+// 		_S( ei_end_shader());
+// 
+// 		_S( ei_material( shader.getName().c_str() ) );
+// 		_S( ei_add_surface(	shaderobject.c_str() ) );
+// 		//_S( ei_shadow( "opaque_shadow" ) );
+// 		_S( ei_end_material() );
 	}
 	liqLightHandle Renderer::shader_light(
 		const liqShader &shader,
@@ -144,4 +165,51 @@ namespace elvishray
 		_S( ei_end_material() );
 		_s("//----------------phong_shader_for_test end ---");
 	}
+	//////////////////////////////////////////////////////////////////////////
+	void getDagPathByName(MDagPath &dagPath, const char *name)
+	{
+		MSelectionList      selList;
+		IfMErrorWarn(MGlobal::getSelectionListByName( name, selList ));
+		IfMErrorWarn(selList.getDagPath( 0, dagPath ));
+	}
+	void getDependNodeByName(MObject &depNode, const char *name)
+	{
+		MSelectionList      selList;
+		IfMErrorWarn(MGlobal::getSelectionListByName( name, selList ));
+		IfMErrorWarn(selList.getDependNode( 0, depNode ));
+	}
+	void getShaderType(MString &type, const MString &name)
+	{
+		IfMErrorWarn( MGlobal::executeCommand( ("nodeType \""+name+"\""), type) );
+	}
+	void shader_surface_lambert(const MString &mayaShaderName)
+	{
+		MDoubleArray color;
+		IfMErrorWarn( MGlobal::executeCommand( ("getAttr \""+mayaShaderName+".color\""), color) );
+		
+		MDoubleArray transparency;
+		IfMErrorWarn( MGlobal::executeCommand( ("getAttr \""+mayaShaderName+".transparency\""), transparency) );
+
+		const MString shaderObjectName(mayaShaderName+"_object");
+		const MString opaqueShadowObjectName(mayaShaderName+"_opaque_shadow_object");
+		_s("//----------------shader begin---");
+		_S( ei_shader( shaderObjectName.asChar()) );
+		_S( ei_shader_param_string("desc", "plastic") );
+		_S( ei_shader_param_vector("Cs", color[0], color[1], color[2]) );
+		_S( ei_shader_param_vector("Kd", 0.7f, 1.0f, 1.0f) );
+		_S( ei_shader_param_scalar("Ks", 1.0f) );
+		_S( ei_shader_param_scalar("roughness", 0.2f) );
+		_S( ei_end_shader() );
+
+		_S( ei_shader(opaqueShadowObjectName.asChar()) );
+		_S( ei_shader_param_string("desc", "opaque") );
+		_S( ei_end_shader() );
+
+		_S( ei_material(mayaShaderName.asChar()) );
+		_S( ei_add_surface(shaderObjectName.asChar()) );
+		_S( ei_add_shadow(opaqueShadowObjectName.asChar()) );
+		_S( ei_end_material() );
+		_s("//----------------shader end ---");
+	}
+	//
 }//namespace elvishray
