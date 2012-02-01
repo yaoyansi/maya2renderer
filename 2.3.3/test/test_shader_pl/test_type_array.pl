9eb0b3c9348ed488de27c4b9bf2c1fb3384c605e
yaoyansi <yaoyansi2@yahoo.com.cn>
2012-02-01 22:39:50 +0800

    liq| mel| : try to call myDeleteAttr() but it always crash. I use the hard code now. see http://www.cnblogs.com/yaoyansi/articles/2335156.html

---
+++ 2.3.3/test/test_shader_pl/test_type_array.pl
@@ -0,0 +1,7 @@
+#shaderType
+surface
+#shaderName
+test_type_array
+#Name:Details:Type:IsOutput:Defaults:ArraySizes
+v0:uniform:vector:0:<<1.000000,5.000000,3.000000>>:-1
+fa0:uniform:float:0:{0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0}:16
