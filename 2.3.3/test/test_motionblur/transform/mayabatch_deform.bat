40853448e0f24c4e18ba347ef7b5e96ca69bfacc
yaoyansi <yaoyansi2@yahoo.com.cn>
2012-02-08 21:28:54 +0800

    liq| test|: motion blur

---
+++ 2.3.3/test/test_motionblur/transform/mayabatch_deform.bat
@@ -0,0 +1,8 @@
+set LIQUID_ROOT=E:\dev\Autodesk\maya\myplugin\project\liquid_
+set TEST_FILE=%LIQUID_ROOT%\2.3.3\test\test_motionblur\transform\deform.ma
+
+mayabatch -file "%TEST_FILE%" -command "source \"registerLiquidRenderer.mel\"; registerLiquidRenderer(); setAttr -type \"string\" liquidGlobals.renderer elvishray; mayaBatchRenderProcedure(0,\"\",\"\",\"liquid\",\"\");" 
+
+mayabatch -file "%TEST_FILE%" -command "source \"registerLiquidRenderer.mel\"; registerLiquidRenderer(); setAttr -type \"string\" liquidGlobals.renderer renderman; mayaBatchRenderProcedure(0,\"\",\"\",\"liquid\",\"\");" 
+
+pause
