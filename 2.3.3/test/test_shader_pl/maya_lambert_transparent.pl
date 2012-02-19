b49d3563e57e0cf16fd27c9cd42ac58725bb8ca1
yaoyansi <yaoyansi2@yahoo.com.cn>
2012-02-19 22:31:29 +0800

    liq| test|: update test case - test_shader_pl.ma

---
+++ 2.3.3/test/test_shader_pl/maya_lambert_transparent.pl
@@ -0,0 +1,8 @@
+#shaderType
+surface
+#shaderName
+maya_lambert_transparent
+#Name:Details:Type:IsOutput:Defaults:ArraySizes
+frequency:uniform:float:0:8.5:-1
+mode:uniform:float:0:0.0:-1
+mode2:uniform:float:0:0.5:-1
