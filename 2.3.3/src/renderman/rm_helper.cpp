6b76f0d75bc96a9c3c84cfaf24b0d04ec453d1af
yaoyansi <yaoyansi2@yahoo.com.cn>
2012-04-05 19:06:29 +0800

    liq| rm|: if ribfile path name >=255, windows will not create this file.

--- 2.3.3/src/renderman/rm_helper.cpp
+++ 2.3.3/src/renderman/rm_helper.cpp
@@ -27,6 +27,13 @@ namespace renderman
 
 		m_ribFile = ribFile;
 
+		//if(m_ribFile=="E:/MyDocuments/maya/projects/default/rib/_Man_JAP_Man_JapSkeleton_Man_hips_Man_spine_Man_spine1_Man_xxy_Man_xxx_Man_leftShoulder_Man_leftArm_Man_leftForearm_Man_leftForeArmRoll_Man_leftHand_Man_joint13_Man_leftHandPinky1_Man_leftHandPinky2_Man_leftHandPinky3_Man_pCube11_Man_pCubeShape11.19.rib"){
+		//}
+		if(m_ribFile.size()>=255){
+			RiArchiveRecord( RI_COMMENT, "ribfile path name is too long(>=255), don't output to file %s", m_ribFile.c_str() );
+			m_ribFile.clear();
+		}
+
 		//if m_ribFileFullPath is "", write the data into the current rib file.
 		if( m_ribFile != "" ){
 			//log
@@ -41,6 +48,10 @@ namespace renderman
 	void Helper::RiEndRef()
 	{
 		CM_TRACE_FUNC("Helper::RiEndRef()");
+		
+		//if(m_ribFile=="E:/MyDocuments/maya/projects/default/rib/_Man_JAP_Man_JapSkeleton_Man_hips_Man_spine_Man_spine1_Man_xxy_Man_xxx_Man_leftShoulder_Man_leftArm_Man_leftForearm_Man_leftForeArmRoll_Man_leftHand_Man_joint13_Man_leftHandPinky1_Man_leftHandPinky2_Man_leftHandPinky3_Man_pCube11_Man_pCubeShape11.19.rib"){
+		//	printf("getit");
+		//}
 
 		if( m_ribFile != "" ){
 			RiEnd();
