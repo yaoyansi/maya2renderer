0d3107998f72ea33c8005a316c0e03832a276eff
yaoyansi <yaoyansi2@yahoo.com.cn>
2012-04-24 21:42:16 +0800

    liq| test|: update er to r1422

--- test/image_diff/e/all/testscene.py
+++ test/image_diff/e/all/testscene.py
@@ -94,7 +94,7 @@ def test_all_scene():
 
     
     testFile=LIQUID_ROOT+"/2.3.3/test/test_mib_ao/mib_amb_occlusion.ma"
-    MyTestOne.test_one_scene(testFile, "elvishray")
+    MyTestOne.test_one_scene(testFile, "")
 
     #testFile=LIQUID_ROOT+"/test/animation_test/Trex/Trex/scenes/TrexAnimation_liq_anim.ma"
     #MyTestOne.test_one_scene(testFile, "elvishray")
